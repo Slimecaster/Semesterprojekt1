1ec8c94b86d75231e46a718adab9a14ea3f919a1
Slimecaster <nd070503@gmail.com>
2023-12-14 13:19:08 +0100

    Hej

---
+++ src/main/java/com/example/semesterprojekt1/Bruger.java
@@ -0,0 +1,39 @@
+package com.example.semesterprojekt1;
+public class Bruger {
+    private String brugernavn;
+    private String kodeord;
+    private String email;
+
+    public Bruger() {
+    }
+
+    public Bruger(String brugernavn, String kodeord, String email) {
+        this.brugernavn = brugernavn;
+        this.kodeord = kodeord;
+        this.email = email;
+    }
+
+    public String getBrugernavn() {
+        return brugernavn;
+    }
+
+    public void setBrugernavn(String brugernavn) {
+        this.brugernavn = brugernavn;
+    }
+
+    public String getKodeord() {
+        return kodeord;
+    }
+
+    public void setKodeord(String kodeord) {
+        this.kodeord = kodeord;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.semesterprojekt1 {
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires java.sql;
 
     opens com.example.semesterprojekt1 to javafx.fxml;
     exports com.example.semesterprojekt1;
